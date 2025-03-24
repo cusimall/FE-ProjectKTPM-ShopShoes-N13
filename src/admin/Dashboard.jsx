196c8e26ca097bf3e8c0c056b525fa7221044caa
hoanghuytoi <hoanghuytoi03@Gmail.com>
2025-03-24 21:17:40 +0700

    Dashboard

---
+++ src/admin/Dashboard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container, Row } from "reactstrap";
+import Chart from "./charts/Chart";
+import LineChart from "./charts/LineChart";
+import "./Dashboard.css";
+const Dashboard = () => {
+  return (
+    <section>
+      <Container>
+        <div className="mt-5">
+          <h4>Thống kê chi tiết</h4>
+        </div>
+        {/* </Row> */}
+        <Row style={{ marginLeft: "150px" }}>
+          <Chart />
+        </Row>
+        <div className="chart">
+          <LineChart />
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default Dashboard;
