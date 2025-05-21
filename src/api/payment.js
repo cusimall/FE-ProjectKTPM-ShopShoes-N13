0a354189b77dcb0058cdc7f339ce1ecba6530ab2
cusimall <leminh21102003@gmail.com>
2025-05-21 12:09:12 +0700

    update api create payment

---
+++ src/api/payment.js
@@ -0,0 +1,18 @@
+import axios from "../axiosConfig";
+
+/**
+ * Create a payment request using VNPAY
+ */
+export const createVnPayPayment = (amount) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const response = await axios({
+        url: `/api/v1/payments/vnpay/create/${amount}`,
+        method: "get",
+      });
+      resolve(response);
+    } catch (error) {
+      reject(error.response ? error.response : error);
+    }
+  });
+
