34dd1f122c8981df4edb49b493b76a951d34ce1a
cusimall <leminh21102003@gmail.com>
2025-05-21 12:03:37 +0700

    update api email comfirm

--- src/api/email.js
+++ src/api/email.js
@@ -16,3 +16,20 @@ export const sendEmail = (emailData) =>
       reject(error.response ? error.response : error);
     }
   });
+
+/**
+ * Send order confirmation email
+ */
+export const sendOrderConfirmation = (orderId, emailData) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const response = await axios({
+        url: `/api/email/order-confirmation/${orderId}`,
+        method: "post",
+        data: emailData,
+      });
+      resolve(response);
+    } catch (error) {
+      reject(error.response ? error.response : error);
+    }
+  });
